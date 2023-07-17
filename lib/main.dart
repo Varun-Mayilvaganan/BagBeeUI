1d19ea3ac2b7415618f66a04eed467ec88619a0d
Varun <varunmayilvaganan11@gmail.com>
2023-07-17 19:14:27 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,18 @@
+import 'package:beeui1/page/homepage.dart';
 import 'package:flutter/material.dart';
-import 'package:beeui1/page/edit_profile.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false, // This line removes the "Debug" banner
-      home: Scaffold(
-        backgroundColor:Colors.grey[300],
-        appBar: AppBar(
-          backgroundColor: Colors.amber[700],
-          leading: IconButton(
-            icon: Icon(Icons.arrow_back,color: Colors.black54,),
-            onPressed: () => {},
-          ),
-          title: Text("Back To Home",
-          style: TextStyle(
-            color: Colors.black,
-          ),
-          ),
-          actions: [
-            IconButton(
-              icon: Icon(Icons.menu,color: Colors.black54,),
-              onPressed: () {
-                // Implement any action when the menu icon is pressed
-                // For example, you can open a menu drawer or show a popup menu
-              },
-            ),
-          ],
-        ),
-        body: Container(
-          child: EditProfile(),
-          ),
-        ),
-      );
-
+      debugShowCheckedModeBanner: false,
+      home: const MyHomePage(),
+    );
   }
 }
