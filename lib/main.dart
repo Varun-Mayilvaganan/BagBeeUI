8d00e65ebd89d383e99b7754ff52ce41242b28b0
Varun <varunmayilvaganan11@gmail.com>
2023-07-19 22:11:38 +0530

    updated pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:beeui1/page/homepage.dart';
+import 'package:beeui1/page/alreadysignin.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const MyHomePage(),
+      home: AlreadySignin()
     );
   }
 }
---
+++ lib/page/alreadysignin.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class AlreadySignin extends StatelessWidget {
+  const AlreadySignin({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          child: Align(
+            alignment: Alignment.topRight,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                IconButton(
+                  onPressed: () {},
+                  icon: Image.asset('assets/images/beebag.webp'),
+                  iconSize: 120,
+                  padding: EdgeInsets.only(right: 30),
+                ),
+                Center(
+                  child: Text(
+                    "The first ever",
+                    style: TextStyle(
+                      fontFamily: 'Urbanist',
+                      fontWeight: FontWeight.w600,
+                      fontSize: 30,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 5), // Add a space between the two Text widgets
+                Center(
+                  child: Text(
+                    "Smart Reusable Bag",
+                    style: TextStyle(
+                      fontFamily: 'Urbanist',
+                      fontWeight: FontWeight.w800,
+                      fontSize: 35,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 5),
+                Image.asset('assets/images/Welcome-Screen-Graphic.jpg',
+                height:300),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
